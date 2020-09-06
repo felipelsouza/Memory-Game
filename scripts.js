7e2ae07e8af8ff3edecb15d04c44068509d99e52
Felipe <felipelsouza@unipam.edu.br>
2020-09-06 14:11:07 -0300

    shuffle cards

--- scripts.js
+++ scripts.js
@@ -1,9 +1,13 @@
 const cards = document.querySelectorAll('.memory-card');
 
+let lockBoard = false;
 let hasFlippedCard = false;
 let firstCard, secondCard;
 
 function flipCard() {
+    if (lockBoard) return;
+    if (this === firstCard) return;
+
     this.classList.add('flip');
 
     if (!hasFlippedCard) {
@@ -15,7 +19,6 @@ function flipCard() {
     }
 
     // second click
-    hasFlippedCard = false;
     secondCard = this;
 
     checkForMatch();
@@ -30,13 +33,31 @@ function checkForMatch() {
 function disableCards() {
     firstCard.removeEventListener('click', flipCard);
     secondCard.removeEventListener('click', flipCard);
+
+    resetBoard();
 }
 
 function unflipCards() {
+    lockBoard = true;
+
     setTimeout(() => {
         firstCard.classList.remove('flip');
         secondCard.classList.remove('flip');
+
+        resetBoard();
     }, 1500);
 }
+
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle() {
+    cards.forEach(card => {
+        let randomPos = Math.floor(Math.random() * 12);
+        card.style.order = randomPos;
+    });
+})();
 
 cards.forEach(card => card.addEventListener('click', flipCard));
